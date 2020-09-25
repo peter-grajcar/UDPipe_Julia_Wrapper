835a0b3d4eef4d4f79907a802d0e1e7909f73a5b
Peter Grajcar <peter.grajcar131@gmail.com>
2020-09-25 20:09:17 +0200

    set latest commit
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"0.1"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/ufal/udpipe.git", "3fd3a2c3a76d74fd3b2dcdd31558884502a418c5"),
-	GitSource("https://github.com/peter-grajcar/UDPipe_Julia_Wrapper", "823a444fb94b5d9f7c722114aecb6b17511aa5ba")	
+	GitSource("https://github.com/peter-grajcar/UDPipe_Julia_Wrapper", "0142bb050f34a55273829919cfd12608351a4917")	
 ]
 
 # Bash recipe for building across all platforms
