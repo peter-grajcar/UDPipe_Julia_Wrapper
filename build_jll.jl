072ba50839b0b4f77ba0805e50a9a19499e1b35b
Peter Grajcar <peter.grajcar131@gmail.com>
2020-09-26 21:53:51 +0200

    update build scripts
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

---
+++ build_jll.jl
@@ -0,0 +1,15 @@
+using BinaryBuilder
+
+json = String(read(ARGS[1]))
+buff = IOBuffer(strip(json))
+objs = []
+while !eof(buff)
+    push!(objs, BinaryBuilder.JSON.parse(buff))
+end
+json_obj = objs[1]
+
+BinaryBuilder.cleanup_merged_object!(json_obj)
+json_obj["dependencies"] = Dependency[dep for dep in json_obj["dependencies"] if !isa(dep, BuildDependency)]
+version = json_obj["version"]
+BinaryBuilder.rebuild_jll_package(json_obj, download_dir="products", upload_prefix="https://github.com/peter-grajcar/UDPipe_Julia_Wrapper/releases/download/v$version")
+
--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "UDPipe"
-version = v"0.1"
+version = v"0.1.0"
 
 # Collection of sources required to complete build
 sources = [
